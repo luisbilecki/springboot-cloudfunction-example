0ac7b81ff5b3ac04b18352d3ad0ded8a1b923e2e
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2021-03-25 20:27:33 -0300

    chore: initialize project

---
+++ src/main/java/com/luisbilecki/cloudfunction/SpringbootCloudFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.luisbilecki.cloudfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCloudFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisbilecki/cloudfunction/SpringbootCloudFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisbilecki.cloudfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
